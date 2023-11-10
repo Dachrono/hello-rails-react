8b2ebae79c7e98e7772d7cc29642458a7e3dd26a
Dachrono <andres_zamorano_@hotmail.com>
2023-11-09 19:49:47 -0600

    Add axios, use provider to access at store, Creeate greeting slice

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,8 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Greetings from './components/Greetings'
 
@@ -20,6 +22,8 @@ function App() {
 }
 
 ReactDOM.render(
-  <App/>,
+  <Provider store={store}>
+    <App/>,
+  </Provider>,  
   document.getElementById('root'),
 );
