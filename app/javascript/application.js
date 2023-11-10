da5ef927cc275db26dd313309621085bf5af7b68
Dachrono <andres_zamorano_@hotmail.com>
2023-11-09 19:23:37 -0600

    Create react router

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,9 +3,20 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Greetings from './components/Greetings'
 
 function App() {
-  return (<h1>Hello World!</h1>);
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greetings />} />
+          <Route path="*" element={<p>Not found</p>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 ReactDOM.render(
