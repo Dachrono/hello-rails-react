0c666b2bf016ac640104e9601e62300aad68020c
Dachrono <andres_zamorano_@hotmail.com>
2023-11-09 19:18:20 -0600

    Create function app and render in the root

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (<h1>Hello World!</h1>);
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
